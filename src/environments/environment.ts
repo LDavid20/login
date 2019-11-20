8a75f8f5c7c2a6c98841bebc3b580c33b2524771
LDavid20 <luis.dcc.98@gmail.com>
2019-11-20 09:40:43 -0600

    'Archivos_que_no_se_subieron'

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCqXPav1i8L9IlRUMh_E2RqCnLi3Tr0tcI',
+    authDomain: 'tiempos-d961a.firebaseapp.com',
+    databaseURL: 'https://tiempos-d961a.firebaseio.com',
+    projectId: 'tiempos-d961a',
+    storageBucket: 'tiempos-d961a.appspot.com',
+    messagingSenderId: '426819557240'
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
